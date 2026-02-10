38e480e7f60c94eb0ad9343fafe8f3131c8fe34a
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:15:43 +0400

    Create task-45-predict-mixed-increment.cpp

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-45-predict-mixed-increment.cpp
@@ -0,0 +1,13 @@
+/*
+ * Task 45: Predict Mixed Pre/Post Increment Output
+ * Output: Result of ++a + a++ expression
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 4 << endl;
+    cout << 3 << endl;
+    return 0;
+}
