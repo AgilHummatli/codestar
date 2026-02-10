e66892e799793cc0a381a79eb415c92d2774eb13
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:17:56 +0400

    Create task-47-predict-compound-operations.cpp

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-47-predict-compound-operations.cpp
@@ -0,0 +1,12 @@
+/*
+ * Task 47: Predict Compound Assignment Output
+ * Output: Final value of p after compound operations
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Value of p is: 3" << endl;
+    return 0;
+}
