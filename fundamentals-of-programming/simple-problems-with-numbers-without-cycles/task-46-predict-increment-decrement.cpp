7a28e5e93c37c8fb7c7e7b8e3903c0f61544f89a
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:16:54 +0400

    Create task-46-predict-increment-decrement.cpp

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-46-predict-increment-decrement.cpp
@@ -0,0 +1,13 @@
+/*
+ * Task 46: Predict Pre-Increment and Post-Decrement Output
+ * Output: Result of ++a + a-- expression
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 4 << endl;
+    cout << 1 << endl;
+    return 0;
+}
