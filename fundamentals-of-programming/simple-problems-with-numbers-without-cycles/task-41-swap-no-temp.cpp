8ce38cb6a3bcdacb29b23b682cd00981985eaef4
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:08:38 +0400

    Add C++ program to swap integers without temp variable
    This program swaps two integers without using a temporary variable.

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-41-swap-no-temp.cpp
@@ -0,0 +1,20 @@
+/*
+ * Task 41: Swap Without Third Variable
+ * Input: Two integers (A, B)
+ * Output: Swapped values without using temp variable
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    cout << a << " " << b;
+    return 0;
+}
