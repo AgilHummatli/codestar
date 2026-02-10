b76542ed299753721e945ef96baa8c68f4279c55
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:11:51 +0400

    Add task 42: Predict Post-Increment Output program
    This file contains a C++ program that demonstrates the output of a post-increment operation.

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-42-predict-output.cpp
@@ -0,0 +1,13 @@
+/*
+ * Task 42: Predict Post-Increment Output
+ * Output: Result of a++ + a expression
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 3 << endl;
+    cout << 2 << endl;
+    return 0;
+}
