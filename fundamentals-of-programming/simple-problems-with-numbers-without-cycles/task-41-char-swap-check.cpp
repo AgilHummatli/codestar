e0461c25a3d2ae8c062fe2b7715598536b262aa2
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:10:18 +0400

    Add character swap check program
    Implement a program to check if character swap is possible using ASCII values.

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-41-char-swap-check.cpp
@@ -0,0 +1,16 @@
+/*
+ * Task 41: Check if Character Swap is Possible
+ * Input: Two characters (A, B)
+ * Output: "Possible" (always possible using ASCII values)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a, b;
+    cin >> a >> b;
+    
+    cout << "Possible";
+    return 0;
+}
