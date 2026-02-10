7e63d9a8659d0f3ed697b556b2b29622e41bd7fe
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:13:16 +0400

    Create task-43-predict-double-increment.cpp

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-43-predict-double-increment.cpp
@@ -0,0 +1,13 @@
+/*
+ * Task 43: Predict Double Post-Increment Output
+ * Output: Result of a++ + a++ expression
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 3 << endl;
+    cout << 3 << endl;
+    return 0;
+}
