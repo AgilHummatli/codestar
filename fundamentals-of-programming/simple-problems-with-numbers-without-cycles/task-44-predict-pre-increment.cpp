5767e9bb6d1e8ab4969491664b6e6a90bdd5b612
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2026-02-10 22:14:33 +0400

    Create task-44-predict-pre-increment.cpp

---
+++ fundamentals-of-programming/simple-problems-with-numbers-without-cycles/task-44-predict-pre-increment.cpp
@@ -0,0 +1,13 @@
+/*
+ * Task 44: Predict Double Pre-Increment Output
+ * Output: Result of ++a + ++a expression
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 5 << endl;
+    cout << 3 << endl;
+    return 0;
+}
